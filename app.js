fca033f2789ef58cc08a9924d4f39e5c4bf4af09
sstern6 <scott.stern06@gmail.com>
2015-10-05 14:55:00 -0700

    Username sign in with chat pop up functioning

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io =require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+var userNames = [];
 
+server.listen(3000);
 
 app.get('/',function(request,response){
 
   response.sendFile(__dirname + '/index.html');
 })
 
-server.listen(3000);
+io.sockets.on('connection',function(socket){
+
+  io.emit('new-user',"A new user has joined the chat");
+
+  socket.on('user-name',function(name){
+    console.log(name)
+    // socket.username = name;
+
+    // userNames.push(socket.username);
+    io.sockets.emit('usernames',userNames);
+    // console.log(userNames);
+  });
+
+  socket.on('send message',function(data){
+    io.sockets.emit('new message',data)
+  });
+
+  socket.on('disconnect',function(socket){
+    io.emit("leave-chat","A user has left the chatroom");
+  });
+
+});
+
+
